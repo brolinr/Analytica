5b9df7ef6d92425acda4b265bd4e2b0dec93e58a
Brolin Remunyanga <remunyangabrolin@gmail.com>
2023-07-02 17:33:40 +0200

    Added static pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_30_095845) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "customers", force: :cascade do |t|
+    t.string "name"
+    t.string "about"
+    t.string "location"
+    t.string "address"
+    t.string "email"
+    t.string "phone"
+    t.boolean "buyer"
+    t.boolean "seller"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
