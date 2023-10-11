924c56d70e25d0ce4de0ce68375bd221aa5bcba9
Brolin Remunyanga <remunyangabrolin@gmail.com>
2023-10-11 21:39:57 +0200

    action mailer hotfix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,6 +90,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'your_domain.com' }
+  config.action_mailer.default_url_options = { host: 'https://analytica-web.onrender.com' }
 
 end
