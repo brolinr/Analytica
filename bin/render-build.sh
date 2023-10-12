9e591d5f8d1904694f66b1586eaa7f21f1c96072
Brolin Remunyanga <remunyangabrolin@gmail.com>
2023-10-12 10:55:52 +0200

    [HOTFIX]

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,6 +4,7 @@ set -o errexit
 
 bundle install
 yarn install
+yarn build
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rake db:migrate:reset
+bundle exec rake db:migrate
